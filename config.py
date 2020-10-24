fa7801c0721ec7390ed5b56f7f19c9a9c37a34f5
Pooshan Vyas <pooshan.vyas@gmail.com>
2020-10-23 18:11:44 -0700

    Implemented logging

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+logs_path = project_path + "/logs"
--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,22 @@
 import logging
+import sys
+import os
+import config
+
 class Logger:
     log = logging.getLogger("TESTING")
 
     def create_logs(self):
         Logger.log.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(formatter)
+        logging.getLogger('testing').addHandler(handler)
+        if not os.path.exists(config.logs_path):
+            os.mkdir(config.logs_path)
+        filehandler = logging.FileHandler(config.logs_path + "/test.log")
+        filehandler.setFormatter(formatter)
+        Logger.log.addHandler(filehandler)
+        Logger.log.propagate = False
